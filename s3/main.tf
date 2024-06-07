3dc40deeccb75278a1cae4ff9118e75bec29a1ce
mikemoroney <141825823+mikemoroney@users.noreply.github.com>
2024-06-07 10:49:50 +0100

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -2,10 +2,16 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+variable "bucket_name" { 
+	type = string
+	default = "mm-460-bucket"
+} 
+
 resource "aws_s3_bucket" "mm_bucket" {
-  bucket  = "mm-460-bucket-name"
+  bucket  = var.bucket_name
   tags    = {
 	  Name          = "MMS3Bucket"
-	  Environment    = "Production"
+	  Environment    = "Test"
   }
 }
