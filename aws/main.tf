b0f22c26d29fc962a9b0c54e6d8d9f322f93eca6
mikemoroney <141825823+mikemoroney@users.noreply.github.com>
2024-06-07 09:52:14 +0100

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -4,13 +4,20 @@ provider "aws" {
 }
 
 variable "instance_name" {
+  type = string
   default = "mmawstf"
 } 
+
+variable "public_ssh_key_name" {
+  default = "mmkeypair"
+}
 
 # Create an EC2 instance
 resource "aws_instance" "mmtf1" {
   ami           = "ami-0d1376ce0abc23950"
   instance_type = "t2.large"
+  key_name = var.public_ssh_key_name
+}
   
   tags {
     Name = var.instance_name
