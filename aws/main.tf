1e0a78d9ac871456107a3967ad6468aa91f4662c
mikemoroney <141825823+mikemoroney@users.noreply.github.com>
2024-11-14 11:57:45 +0000

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -17,9 +17,16 @@ resource "aws_instance" "mm_orpheus_ubuntu_micro" {
   ami           = "ami-0b5e44cb401c67ac3"
   instance_type = "t2.small"
   key_name = var.public_ssh_key_name
-  subnet_id = "mm_subnet_01"
+  subnet_id = "subnet-001c5057a706f8dbd"
+  tags = { 
+    Name = var.instance_name
+  }
 }
 
 output "ip_address" { 
-  value = aws_instance.orpheus_ubuntu_micro.private_ip 
+  value = aws_instance.mm_orpheus_ubuntu_micro.private_ip 
+}
+
+output "instance_id" { 
+  value = aws_instance.mm_orpheus_ubuntu_micro.id
 }
